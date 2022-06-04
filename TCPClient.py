34946705737b900a94de771e73dc574f346758f9
Shell Raiser <ksshailesh05@gmail.com>
2022-06-04 14:56:12 +0530

    create tcp client from vid 3

---
+++ TCPClient.py
@@ -0,0 +1,13 @@
+from email import message
+import socket
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname()
+port = 444
+
+clientsocket.connect((host, port))
+message = clientsocket.recv(1024)
+
+clientsocket.close()
+print(message.decode('ascii'))
