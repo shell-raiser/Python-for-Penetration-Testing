fc7e4576c222fa2e65de34401fbd7c35b835d98c
Shell Raiser <ksshailesh05@gmail.com>
2022-06-04 17:44:27 +0530

    added code for comprehensive scan

--- scanner.py
+++ scanner.py
@@ -14,7 +14,7 @@ resp = input(""" \n Enter the type of scan to run
                     3)Comprehensive Scan \n""")    
 print("You have selected: ", resp)                    
 
-if resp == '1':
+if resp == '1': #TCP scan
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan(ip_addr, '1-1024', '-v -sS')
     print(scanner.scaninfo())
@@ -23,7 +23,7 @@ if resp == '1':
 
     # display open ports, with keys() method
     print("Open Ports are: ", scanner[ip_addr]['tcp'].keys())
-elif resp == '2':
+elif resp == '2': #UDP Scan
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan(ip_addr, '1-1024', '-v -sU') # U indicates UDP
     print(scanner.scaninfo())
@@ -32,3 +32,14 @@ elif resp == '2':
 
     # display open ports, with keys() method
     print("Open Ports are: ", scanner[ip_addr]['udp'].keys())
+elif resp == '3': #Comprehensive scan
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print("IP status: ", scanner[ip_addr].state()) #know if online or offline
+    print(scanner[ip_addr].all_protocols())
+
+    # display open ports, with keys() method
+    print("Open Ports are: ", scanner[ip_addr]['tcp'].keys())
+else: #for every other error
+    print("Enter Valid Option")
