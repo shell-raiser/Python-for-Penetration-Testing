d3e1d45fde9a4378704abfe47c0943948d73f44f
Shell Raiser <ksshailesh05@gmail.com>
2022-06-04 17:40:12 +0530

    added UDP scan code

--- scanner.py
+++ scanner.py
@@ -14,3 +14,21 @@ resp = input(""" \n Enter the type of scan to run
                     3)Comprehensive Scan \n""")    
 print("You have selected: ", resp)                    
 
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print("IP status: ", scanner[ip_addr].state()) #know if online or offline
+    print(scanner[ip_addr].all_protocols())
+
+    # display open ports, with keys() method
+    print("Open Ports are: ", scanner[ip_addr]['tcp'].keys())
+elif resp == '2':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sU') # U indicates UDP
+    print(scanner.scaninfo())
+    print("IP status: ", scanner[ip_addr].state()) #know if online or offline
+    print(scanner[ip_addr].all_protocols())
+
+    # display open ports, with keys() method
+    print("Open Ports are: ", scanner[ip_addr]['udp'].keys())
